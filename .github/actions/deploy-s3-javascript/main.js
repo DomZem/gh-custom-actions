88f2e54af679a75f54f106c41e3a0d372f2609f3
DomZem <dominik.zemlik.dev@gmail.com>
2025-09-17 20:02:26 +0200

    ci: added checkout code step to information step

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 
+// Custom JS will not install dependencies, we must include node_modules
 function run() {
 	core.notice('Hello from my custom JavaScript action!');
 }
